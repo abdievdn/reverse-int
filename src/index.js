1d9d6ce8675fe931fc79db683fa40bb9cde4dea9
Dzmitry_A <abdiev.dn@yandex.by>
2020-10-17 14:36:12 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+    let arr = n.toString();
+    let arrResult = [];
+    for (let i = arr.length - 1; i >= 0; i--) {
+        if (arr[i] === '-') break;
+        arrResult.push(arr[i]);
+    }
+
+    return parseInt(arrResult.join(''));
 }
+
+
